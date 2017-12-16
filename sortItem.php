1d90ceaa160f9af506c3585b60edd50a61d17870
Syai97 <syaifikri97@gmail.com>
2017-12-16 14:27:10 +0800

    Add error message

--- sortItem.php
+++ sortItem.php
@@ -7,11 +7,18 @@
   $sql = "SELECT * FROM item WHERE itemname LIKE '%$itemname%'";
   $query = mysqli_query($con, $sql);
 
+
+  if(mysqli_num_rows($query)<1){
+      echo "Sorry, the item you have searched does not exist.";
+    }
+
   while($row = mysqli_fetch_array($query)){
+
+    
     $itemid  = $row['itemid'];
-     echo "<a href='index.php?page=itemInfo&itemId=$itemid'>";
-     echo $row['itemname'];
-     echo "</a><br>";
+    echo "<a href='index.php?page=itemInfo&itemId=$itemid'>";
+    echo $row['itemname'];
+    echo "</a><br>";
   }
 
 
